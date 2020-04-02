70d7eebf47ad0bfd6f29fc1e6284d29d2107be3d
YooHyuk Lee <azsxd34@naver.com>
2020-04-03 03:01:40 +0900

    process chrome url for each tabs

--- memento/background.js
+++ memento/background.js
@@ -1,117 +1,45 @@
+var turnoff = false;
+var user = "";
+
+chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
+    if (turnoff) { return }
+
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined) {
+        console.log("Now login user " + user);
+        console.log("Now tab id " + typeof(tabId));
+        console.log("Now tab title " + tab.title);
+        console.log("Now tab url " + tab.url);
+
+        sendPost(data = {
+            tabId: tabId,
+            title: tab.title,
+            url: tab.url,
+            visitedTime: new Date().getTime() / 1000,
+            stayedTime: 10000000
+        });
+    }
+}); 
+
 chrome.extension.onConnect.addListener(function(port) {
     console.log("Connected .....");
     port.onMessage.addListener(function(msg) {
         console.log("message recieved " + msg);
         if (msg == 'On') {
-            console.log('워커 시작');
+            console.log('이벤트 감지 시작');
             Login();
-            startWorker();
+            turnoff = false;
         }
         else if (msg == 'Off'){
-            console.log('워커 종료');
-            stopWorker();
+            console.log('이벤트 감지 종료');
+            turnoff = true;
         }
     });
 });
 
-var interval;
-function startWorker() {
-    if (!interval){
-        console.log('start 실행중인 인터벌 종료');
-        clearInterval(interval);
-    }
-    interval = setInterval( function() {
-        buildTypedUrlList();
-    }, 10000 );
-
-}
-
-function stopWorker() {
-    if(interval){
-        console.log('stop 실행중인 인터벌 종료');
-        clearInterval(interval);
-    }
-}
-
-// function onAnchorClick(event) {
-//     chrome.tabs.create({
-//         selected: true,
-//         url: event.srcElement.href
-//     });
-//     return false;
-// }
-
-
-// 기록을 검색하여 사용자가 입력한 링크를 최대 10개 찾아 팝업으로 표시
-function buildTypedUrlList() {
-    // 지난주의 히스토리 아이템을 찾기 위해, 현재 시간에서 일주일을 뺀다.
-    var searchInterval = (new Date).getTime() - 1000 * 10 * 1.01;
-
-    // chrome.history.getVisits() 에서 받기를 원하는 callback 수 추적
-    // 0에 도달할 경우, 모든 결과를 얻는다
-    var numRequestsOutstanding = 0;
-
-    chrome.history.search({
-        'text': '',              // 모든 히스토리 아이템을 가져온다
-        'startTime': searchInterval  // 10초 이내
-    },
-        function(historyItems) {
-        // 각각의 히스토리로부터 모든 방문 세부사항을 얻음
-            let data = [];
-            for (var i = 0; i < historyItems.length; ++i) {
-                let title = historyItems[i].title;
-                let lastVisitTime = historyItems[i].lastVisitTime;
-                let visitCount = historyItems[i].visitCount;
-                let userUrl = historyItems[i].url;
-
-                console.log('----------------------------');
-                console.log(title);
-                console.log(lastVisitTime);
-                console.log(visitCount);
-                console.log(userUrl);
-                console.log('----------------------------');
-                // let processVisitsWithUrl = function(url) {
-                //     return function(visitItems) {
-                //         processVisits(url, visitItems);
-                //         };
-                // };
-                let datasnippet = {
-                    title: title,
-                    lastVisitTime: lastVisitTime,
-                    visitCount: visitCount,
-                    userUrl: userUrl
-                }
-                data.push(datasnippet);
-                numRequestsOutstanding++; // 히스토리 개수
-            }
-            if (numRequestsOutstanding) {
-                sendPost(data);
-            }}
-    );
-
-    // var onAllVisitsProcessed = function() {
-    //     console.log();
-    //     // 팝업에 추가
-    // };
-    // var urlToCount = [];
-    // // chrome.history.getVisits() 콜백.
-    // var processVisits = function(url, visitItems) {
-    //     for (var i = 0, ie = visitItems.length; i < ie; ++i) {
-    //         let tmp = "type : " + visitItems[i].transition + " | url : " + url
-    //         urlToCount.push(tmp);
-    //     }
-    //     // 만일 이것이 ProcessVisits()에 대한 최종 outstanding 호출이라면, 최종 결과를 얻는다. 
-    //     // 팝업에 표시할 URL 목록을 작성 진행
-    //     if (!--numRequestsOutstanding) {
-    //         onAllVisitsProcessed();
-    //     }
-    // };
-
-    // 디스플레이 할 최종 url 리스트가 있을 경우 호출됨
-}
 function Login() {
     chrome.identity.getProfileUserInfo(function(userInfo) {
-        var url = "http://localhost:8080/save-user?name=" + userInfo.email;
+        user = userInfo.email;
+        var url = "http://localhost:8080/save-user?name=" + user;
         var xhr = new XMLHttpRequest();
         xhr.open("POST",url ,true);
         xhr.setRequestHeader('Content-Type',  'application/x-www-form-urlencoded');
@@ -120,16 +48,9 @@ function Login() {
 }
 
 function sendPost(data) {
-<<<<<<< HEAD
-    console.log(JSON.stringify(data));
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://localhost:8080/collect', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data));
-=======
     var destUrl;
     chrome.identity.getProfileUserInfo(function(userInfo) {
-        destUrl = 'http://localhost:8080/collect?name=' +userInfo.email;
+        destUrl = 'http://localhost:8080/collect?name=' + user;
         console.log(destUrl);
         console.log(JSON.stringify(data));
         var xhr = new XMLHttpRequest();
@@ -137,5 +58,4 @@ function sendPost(data) {
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify(data));
     });
->>>>>>> 00a0503907e4a1c2783ca8377a1e1e8c106b4d90
 }
--- src/test/java/com/memento/web/service/SearchServiceTest.java
+++ src/test/java/com/memento/web/service/SearchServiceTest.java
@@ -26,6 +26,7 @@ class SearchServiceTest {
     private static final String title1 = "test1 - Google 검색";
     private static final String title2 = "test2 : 네이버 통합검색";
     private static final String title3 = "test3 : 네이버 통합검색";
+    private static final String title4 = "4tset : 네이버 통합검색";
     private final String user = "test-user";
 
     @Autowired
@@ -50,6 +51,8 @@ class SearchServiceTest {
         requestDtoList.add(HistoryRequestDto.builder().title("test2-1").userUrl("http//springboot2-1.com").lastVisitTime(1585405292000L).visitCount(1).build());
         requestDtoList.add(HistoryRequestDto.builder().title(title3).userUrl("http//springboot3.com").lastVisitTime(1585405292000L).visitCount(1).build());
         requestDtoList.add(HistoryRequestDto.builder().title("test3-1").userUrl("http//springboot3-1.com").lastVisitTime(1585405292000L).visitCount(1).build());
+        requestDtoList.add(HistoryRequestDto.builder().title(title4).userUrl("http//springboot4.com").lastVisitTime(1585405292000L).visitCount(1).build());
+        requestDtoList.add(HistoryRequestDto.builder().title("test4-1").userUrl("http//springboot4-1.com").lastVisitTime(1585405292000L).visitCount(1).build());
 
         //save test user
         userRepository.save(User.builder().name("test-admin").id(ObjectId.get().toString()).build());
@@ -70,7 +73,7 @@ class SearchServiceTest {
 
         assertEquals(responseDtos.size(), 2);
         assertEquals(responseDtos.get(1).getKeyword(), "test2");
-        assertEquals(responseDtos1.size(), 1);
+        assertEquals(responseDtos1.size(), 2);
         assertEquals(responseDtos1.get(0).getKeyword(), "test3");
         assertEquals(responseDtos2.size(), 1);
         assertEquals(responseDtos2.get(0).getKeyword(), "test3");
@@ -80,7 +83,7 @@ class SearchServiceTest {
     void 검색어_페이지별_반환() {
         List<HistoryResponseDto> responseDtos = searchService.findAllByKeyword(user, "test", 1, 2);
         List<HistoryResponseDto> responseDtos1 = searchService.findAllByKeyword(user, "test", 2, 1);
-        List<HistoryResponseDto> responseDtos2 = searchService.findAllByKeyword(user, "test", 1, 3);
+        List<HistoryResponseDto> responseDtos2 = searchService.findAllByKeyword(user, "test", 1, 4);
 
         assertEquals(responseDtos.size(), 2);
         assertEquals(responseDtos.get(1).getKeyword(), "test2");
